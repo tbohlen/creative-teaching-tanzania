df24f1f7c80f1300aa1e3982819993c9c3919017
Turner Bohlen <turnerbohlen@gmail.com>
2017-06-29 11:24:42 +0300

    Minor fixes

--- src/client/components/gallery.jsx
+++ src/client/components/gallery.jsx
@@ -2,12 +2,13 @@ import React, { Component } from "react";
 import GalleryImage from "./gallery-image";
 import styles from "../styles/gallery.css";
 
-const IMAGES = ["/images/workshop-1.png", "/images/workshop-2.png", "/images/workshop-3.png", "/images/workshop-4.png", "/images/workshop-5.png", "/images/workshop-6.png", "/images/workshop-7.png", "/images/workshop-8.png", "/images/workshop-9.png", "/images/workshop-10.png", "/images/workshop-11.png", "/images/workshop-12.png", "/images/workshop-13.png"];
+const IMAGES = ["/images/workshop-1.png", "/images/workshop-2.png", "/images/workshop-3.png", "/images/workshop-4.png", "/images/workshop-5.png", "/images/workshop-6.png", "/images/workshop-7.png", "/images/workshop-8.png", "/images/workshop-9.png", "/images/workshop-10.png", "/images/workshop-11.png", "/images/workshop-12.png", "/images/workshop-13.png", "/images/workshop-14.png"];
 class Gallery extends Component {
     render() {
         return (
             <div className={ styles.wrapper }>
                 <div className={ styles.innerWrapper }>
+                    <h1 className={ styles.title }>Photos</h1>
                     { IMAGES.map((url, id) => {
                         return <GalleryImage url={ url } key={ id } />;
                     })}
